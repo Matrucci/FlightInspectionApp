2352e21b68a70e957dacb16900ae7873acf38b6a
Matrucci <matan.saloniko@gmail.com>
2021-04-03 12:23:39 +0300

    first commit - groundwork for the first userstory

---
+++ FlightInspectionApp/FlightInspectionApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace FlightInspectionApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IObservable, IView
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public event Update notify;
+
+        public int getUserCommand()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
